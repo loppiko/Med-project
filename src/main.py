da9046af4270dcbefed4e7f7fc10b3b18568e1ee
loppiko <insertthemontana@gmail.com>
2024-08-07 17:34:15 +0200

    add readme

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from planResolver import PlanResolver
+
+
+professionFilePath = "database/professionsList.json"
+scheduleFilePath = "database/schedule.json"
+workersFilePath = "database/workers.json"
+
+resolver = PlanResolver(professionFilePath, workersFilePath, scheduleFilePath)
+resolver.initializePygad()
